1974ab8bead720ea7c75b2c189281d61fb83d8ef
paramita22 <74617780+paramita22@users.noreply.github.com>
2023-04-05 17:55:20 +0530

    Add files via upload

--- src/main/java/in/onlinebookshop/dao/BookDaoImpl.java
+++ src/main/java/in/onlinebookshop/dao/BookDaoImpl.java
@@ -1,18 +1,21 @@
 package in.onlinebookshop.dao;
 
 import java.io.IOException;
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
 
-import in.ineuron.util.BookUtil;
+import in.onlinebookshop.util.BookUtil;
+import in.onlinebookshop.dto.Book;
 
 public class BookDaoImpl implements IBookDao {
 
 	Connection connection = null;
-
+	String status1="";
+	
 	@Override
 	public String save(Book book) {
 		
@@ -20,7 +23,7 @@ public class BookDaoImpl implements IBookDao {
 		PreparedStatement pstmt = null;
 		String status = null;
 		try {
-			connection = JdbcUtil.getJdbcConnection();
+			connection = BookUtil.getJdbcConnection();
 			if (connection != null)
 				pstmt = connection.prepareStatement(sqlInsertQuery1);
 			if (pstmt != null) {
@@ -39,52 +42,54 @@ public class BookDaoImpl implements IBookDao {
 					status = "failure";
 				}
 			}
-			
-			String sqlInsertQuery2 = "insert into `old_books`(`old_bookid`,`old_book_name`,`authorname`,`hardcover`,`online_version`,`other_formats`,`cost`,`donor_id`,`book_cover`) values(?,?,?,?,?,?,?,?,?)";
+		}
+		catch(Exception e) {
+			System.out.println(e);
+		}
+		String sqlInsertQuery2 = "insert into `old_books`(`old_bookid`,`old_book_name`,`authorname`,`hardcover`,`online_version`,`other_formats`,`cost`,`donor_id`,`book_cover`) values(?,?,?,?,?,?,?,?,?)";
 		PreparedStatement pstmt1 = null;
-		String status = null;
+		String status1 = null;
 		try {
-			connection = JdbcUtil.getJdbcConnection();
+			connection = BookUtil.getJdbcConnection();
 			if (connection != null)
 				pstmt1 = connection.prepareStatement(sqlInsertQuery1);
 			if (pstmt1 != null) {
 				pstmt1.setInt(1, book.getOldBookId());
-				pstmt1.setString(2, book.getUserName());
+				pstmt1.setString(2, book.getBookName());
 				pstmt1.setString(3, book.getAuthorName());
 				pstmt1.setString(4, book.getHardcover_copies());
 				pstmt1.setString(5, book.getOnlineVersions());
 				pstmt1.setString(6, book.getOtherFormats());
 				pstmt1.setString(7, book.getCost());
-				pstmt1.setString(8, book.getDonorId());
+				pstmt1.setString(8, book.getDonor_id());
 			}
 			if (pstmt1 != null) {
 				int rowAffected = pstmt1.executeUpdate();
 				if (rowAffected == 1) {
-					status = "success";
+					status1 = "success";
 				} else {
-					status = "failure";
+					status1 = "failure";
 				}
 			}
 
 		} catch (SQLException | IOException e) {
 			e.printStackTrace();
-			status = "failure";
+			status1 = "failure";
 		}
-		return status;
+		return status1;
 	}
-
-	@Override
-	public Book findById(Integer sid) {
+		
+	public String findById(int nbid) {
 
 		String sqlSelectQuery = "select nbid,book_name,author_name,hardcover_copies,online_versions,cost,book_cover from new_books where nbid=?";
 		PreparedStatement pstmt = null;
 		Book book = null;
 		try {
-			connection = JdbcUtil.getJdbcConnection();
+			connection = BookUtil.getJdbcConnection();
 			if (connection != null)
 				pstmt = connection.prepareStatement(sqlSelectQuery);
 			if (pstmt != null) {
-				pstmt.setInt(1, sid);
+				pstmt.setInt(1, nbid);
 			}
 			if (pstmt != null) {
 				ResultSet resultSet = pstmt.executeQuery();
@@ -97,8 +102,8 @@ public class BookDaoImpl implements IBookDao {
 					book.setBookName(resultSet.getString(2));
 					book.setAuthorName(resultSet.getString(3));
 					book.setHardcover_copies(resultSet.getString(4));
-					book.setOnlineVersions(resultSet.getString(5));
-					book.setCost(resultSet.getString());
+					book.setOnlineversions(resultSet.getString(5));
+					book.setCost(resultSet.getString(6));
 					
 				}
 			}
@@ -106,25 +111,28 @@ public class BookDaoImpl implements IBookDao {
 		} catch (SQLException | IOException e) {
 			e.printStackTrace();
 		}
+		String status = null;
 		return status;
 	}
 
 	@Override
-	public String updateById(Student student) {
+	public String updateById(Book book) {
 		String sqlUpdateQuery1 = "update new_books set nbid=?,book_name=?,author_name=?,hardcover_copies=?,online_versions=?,cost=? where nbid = ?";
 		PreparedStatement pstmt = null;
+		
 		String status = null;
 		try {
-			connection = JdbcUtil.getJdbcConnection();
+			connection = BookUtil.getJdbcConnection();
 			if (connection != null)
-				pstmt = connection.prepareStatement(sqlUpdateQuery);
+				pstmt = connection.prepareStatement(sqlUpdateQuery1);
 			if (pstmt != null) {
-				book.setNbid(resultSet.getInt(1));
+					ResultSet resultSet = null;
+					book.setNbid(resultSet.getInt(1));
 					book.setBookName(resultSet.getString(2));
 					book.setAuthorName(resultSet.getString(3));
 					book.setHardcover_copies(resultSet.getString(4));
-					book.setOnlineVersions(resultSet.getString(5));
-					book.setCost(resultSet.getString());
+					book.setOnlineversions(resultSet.getString(5));
+					book.setCost(resultSet.getString(6));
 				
 			}
 			if (pstmt != null) {
@@ -143,20 +151,20 @@ public class BookDaoImpl implements IBookDao {
 	}
 
 	@Override
-	public String deleteById(Integer nbid {
+	public String deleteById(int nbid) {
 
 		String sqlDeleteQuery = "delete from new_books where nbid = ? ";
 		PreparedStatement pstmt = null;
 		String status = null;
 		try {
-			Book book = findById(nbid);
-			if (student != null) {
+			String bk = findById(nbid);
+			if (bk != null) {
 
-				connection = JdbcUtil.getJdbcConnection();
+				connection = BookUtil.getJdbcConnection();
 				if (connection != null)
 					pstmt = connection.prepareStatement(sqlDeleteQuery);
 				if (pstmt != null)
-					pstmt.setInt(1, sid);
+					pstmt.setInt(1, nbid);
 
 				if (pstmt != null) {
 					int rowAffected = pstmt.executeUpdate();
@@ -173,5 +181,6 @@ public class BookDaoImpl implements IBookDao {
 		}
 		return status;
 	}
+
 
 }
--- src/main/java/in/onlinebookshop/dao/IBookDao.java
+++ src/main/java/in/onlinebookshop/dao/IBookDao.java
@@ -1,12 +1,16 @@
 package in.onlinebookshop.dao;
 
-import in.ionlinebookshop.dto.Book;
+import in.onlinebookshop.dto.Book;
 
 public interface IBookDao {
 	String save(Book book);// Creating a record
-	Book findById(int nbid);// Reading a record
-	String findById(int old_bookid);
+	String findById(int nbid);// Reading a record
+
 	String updateById(Book book);// Updating a record
 	String deleteById(int nbid);
 	// Deleting a record
+
+	
+	
+	
 }
